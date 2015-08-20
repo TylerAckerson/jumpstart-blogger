018924ba909c7f21aa697f0f58eea4d551e3df9e
Tyler Ackerson <tackersonMSU12@gmail.com>
2015-08-20 10:23:20 -0700

    Sorcery auth complete

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,4 +1,5 @@
 class TagsController < ApplicationController
+  before_filter :require_login, only: [:destroy]
 
   def show
     @tag = Tag.find(params[:id])
@@ -16,5 +17,5 @@ class TagsController < ApplicationController
 
     redirect_to tags_path
   end
-  
+
 end

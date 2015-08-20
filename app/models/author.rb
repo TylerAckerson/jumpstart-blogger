6827a003551dec602930b84a4ed986cc2ec069c2
Tyler Ackerson <tackersonMSU12@gmail.com>
2015-08-20 11:14:32 -0700

    some UI cleanup and username/email/password validation

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,4 +1,14 @@
 class Author < ActiveRecord::Base
   authenticates_with_sorcery!
   validates_confirmation_of :password, message: "should match confirmation", if: :password
+
+  validates :username, presence: true
+  validates :username, uniqueness: true
+
+  validates :email, presence: true
+  validates :email, uniqueness: true
+
+  validates :password, length: { minimum: 3 }
+  validates :password, confirmation: true
+  validates :password_confirmation, presence: true#
 end
